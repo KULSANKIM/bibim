141fbd4cbc61d0b0ab545796198d7c9dcf851eb9
DESKTOP-4MNU2N5\LG <kimsh82790@gmail.com>
2023-05-21 17:05:04 +0900

    mustache 삭제기능 구현

--- src/main/java/com/springboot/service/posts/PostsService.java
+++ src/main/java/com/springboot/service/posts/PostsService.java
@@ -47,4 +47,12 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
